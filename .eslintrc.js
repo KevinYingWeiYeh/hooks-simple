e0a38bc2e3e9adc17707948681544be6050e7cdb
Kevin Yeh <revinyeh@gmail.com>
2019-03-16 21:26:31 -0700

    hooked up the todo & posts  buttons with AIP call

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,18 +17,22 @@ module.exports = {
       "sourceType": "module"
   },
   "plugins": [
-      "react"
+      "react",
+      "jsx-a11y",
+      "import"
   ],
   "rules": {
     "react/jsx-uses-react": "error",
     "react/jsx-uses-vars": "error",
-    "indent": [
-      "error",
-      2
-    ],
+    "indent": ["error",2],
+    "react/jsx-filename-extension": 0,
+    "semi": 0,
+    "react/jsx-indent": 0,
+    "jsx-a11y/img-redundant-alt": 0
   },
   "extends": [
     "eslint:recommended",
     "plugin:react/recommended"
-  ]
+  ],
+  "parser": "babel-eslint",
 };
