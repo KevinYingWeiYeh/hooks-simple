37d6b2dda67114545fea9eecfa1ddab3ec53cbe5
Kevin Yeh <revinyeh@gmail.com>
2019-03-16 23:16:47 -0700

    used setEffect hooks to link Button and Field components

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -1,25 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-class ResourceList extends React.Component {
-  state = { resources: [] }
-
-  async componentDidMount() {
-    const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`);
-    this.setState({ resources: response.data})
+const ResourceList = ({ resource }) => {
+  const [resources, setResources] = useState([]);
+  const fetchResource = async (resource) => {
+    const response = await axios.get(`http://jsonplaceholder.typicode.com/${resource}`);
+    setResources(response.data)
   }
 
-  async componentDidUpdate(prevProps) {
-    if(prevProps.resource !== this.props.resource) {
-      const response = await axios.get(`http://jsonplaceholder.typicode.com/${this.props.resource}`);
-      this.setState({ resources: response.data })
-    }
-  }
-
-  render() {
-    /* eslint react/prop-types: 0 */
-    return <div>{this.state.resources.length}</div>;
-  }
+  useEffect(() => {
+    fetchResource(resource)
+  }, [resource])
+  /* eslint react/prop-types: 0 */
+  return <div>{resources.length}</div>;
 }
 
 export default ResourceList;
